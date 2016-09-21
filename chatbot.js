e5c5cd14e320fe54dee81db5c549a30400df7fe3
Owen Vandevelde <owen.vnade@gmail.com>
2016-09-21 07:43:53 -0700

    responds to certain things

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,23 @@
-var question
-var chatBox
+var question;
+var input;
+var response = "FRIEND BOT: Hello";
+
 function run1() {
-question = document.getElementById("input").value;
-document.getElementById("chat-area").textContent += question + "\n";
+input = document.getElementById("input").value;
+
+var n = input.search(/how are you/i);
+
+if(input == "hi" || input == "hello" || input == "Hi" || input == "Hello"){
+   document.getElementById("chat-area").innerHTML += response + "<br>";
+}
+else if(n > -1){
+  response = "FRIEND BOT: Good";
+  document.getElementById("chat-area").innerHTML += response + "<br>";
+  response = "FRIEND BOT: Hello";
+}
+
+question = "USER: " + input + "<br>"
+document.getElementById("chat-area").innerHTML += question;
+
+document.getElementById("input").value = null;
 }
